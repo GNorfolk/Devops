636daa61afd91b6e70a631fa8f28511e075ce717
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-10-19 11:05:49 +0100

    Made provision.sh exe and added old folders.

--- Vagrantfile
+++ Vagrantfile
@@ -12,4 +12,10 @@ Vagrant.configure("2") do |config|
   config.vm.network "private_network", ip: "192.168.10.100"
   config.hostsupdater.aliases = ["development.local"]
 
+  # sync the app folder to the guest
+  config.vm.synced_folder "app", "/home/ubuntu/app"
+
+  # run the app provisioning script
+  config.vm.provision "shell", path: "environment/app/provision.sh"
+
 end
---
+++ environment/app/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# update the sources list and upgrade any existing packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install correct version of node.js
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install pm2 with npm
+sudo npm install pm2 -g

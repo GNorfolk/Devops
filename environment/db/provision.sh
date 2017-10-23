95f9c17e1628e52f4e33832190cc730f3bd7ed59
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-10-23 15:34:20 +0100

    Fixed provision file for connection error.

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -27,4 +27,5 @@ sudo rm /etc/mongod.conf
 sudo cp /home/ubuntu/environment/db/mongod.conf /etc/mongod.conf
 
 # Resets mondo service
-sudo service mongod restart
+sudo service mongodb enable
+sudo service mongodb restart

f30f5540bdbe82ac580aa98452805245b524b161
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-11-09 15:44:29 +0000

    Edited terraform.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,68 +1,133 @@
 provider "aws" {
-	region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 # VPC
 resource "aws_vpc" "uat" {
-	cidr_block = "10.5.0.0/16"
+  cidr_block = "10.5.0.0/16"
 
-	tags {
-		Name = "uat-george"
-	}
+  tags {
+    Name = "uat-george"
+  }
 }
 
-# application inastance
+# application instance
 resource "aws_instance" "app" {
-	ami = "ami-3ef4e85a"
-	instance_type = "t2.micro"
-	subnet_id = "${aws_subnet.public.id}"
-	security_groups = ["${aws_security_group.app.id}"]
-	user_data = "${data.template_file.init.rendered}"
+  ami = "ami-9af5e9fe"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.public.id}"
+  security_groups = ["${aws_security_group.app.id}"]
+  user_data = "${data.template_file.init.rendered}"
 
-	tags {
-		Name = "app-george"
-	}
+  tags {
+    Name = "app-george"
+  }
 
-	lifecycle {
-		create_before_destroy = true
-	}
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # database instance
 resource "aws_instance" "db" {
-	ami = "ami-45fde121"
-	instance_type = "t2.micro"
-	subnet_id = "${aws_subnet.private.id}"
-	security_groups = ["${aws_security_group.db.id}"]
+  ami = "ami-45fde121"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.private.id}"
+  security_groups = ["${aws_security_group.db.id}"]
 
-	tags {
-		Name = "db-george"
-	}
+  tags {
+    Name = "db-george"
+  }
 
-	lifecycle {
-		create_before_destroy = true
-	}
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create a new load balancer
+resource "aws_elb" "elb" {
+  name               = "alb-george"
+  availability_zones = ["eu-west-2a"]
+  subnets = ["",""]
+  vpc_id = "${aws_vpc.uat.id}"
+
+  # access_logs {
+  #   bucket        = "foo"
+  #   bucket_prefix = "bar"
+  #   interval      = 60
+  # }
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  # listener {
+  #   instance_port      = 8000
+  #   instance_protocol  = "http"
+  #   lb_port            = 443
+  #   lb_protocol        = "https"
+  #   ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
+  # }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 10
+  }
+
+  instances                   = ["${aws_instance.app.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "elb-george"
+  }
 }
 
 # private subnet for db
-resource "aws_subnet" "private" {
-	vpc_id = "${aws_vpc.uat.id}"
-	cidr_block = "10.5.0.0/24"
+resource "aws_subnet" "db" {
+  vpc_id = "${aws_vpc.uat.id}"
+  cidr_block = "10.5.0.0/24"
+  availability_zone = "eu-west-2a"
 
-	tags {
-		Name = "private-george"
-	}
+  tags {
+    Name = "db-george"
+  }
 }
 
-# public subnet for app 
-resource "aws_subnet" "public" {
-	vpc_id = "${aws_vpc.uat.id}"
-	cidr_block = "10.5.1.0/24"
-	map_public_ip_on_launch = true
+# ~~public~~ private subnet for app 
+resource "aws_subnet" "app" {
+  vpc_id = "${aws_vpc.uat.id}"
+  cidr_block = "10.5.1.0/24"
+  availability_zone = "eu-west-2a"
+  # map_public_ip_on_launch = true
 
-	tags {
-		Name = "public-george"
-	}
+  tags {
+    Name = "app-george"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# public subnet for elb
+resource "aws_subnet" "elb" {
+  vpc_id = "${aws_vpc.uat.id}"
+  cidr_block = "10.5.2.0/24"
+  availability_zone = "eu-west-2a"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "elb-george"
+  }
 }
 
 # Template for initial configuration bash script
@@ -70,40 +135,33 @@ data "template_file" "init" {
   template = "${file("templates/init.sh.tpl")}"
 
   vars {
-  	db_host_ip = "${aws_instance.db.private_ip}"
+    db_host_ip = "${aws_instance.db.private_ip}"
   }
 }
 
 # security group for the app
 resource "aws_security_group" "app" {
-	name        = "app-george"
-	description = "Security group for the app"
-	vpc_id      = "${aws_vpc.uat.id}"
+  name        = "app-george"
+  description = "Security group for the app"
+  vpc_id      = "${aws_vpc.uat.id}"
 
-	ingress {
-		from_port   = 80
-		to_port     = 80
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.elb.id}"]
+  }
 
-	ingress {
-		from_port   = 443
-		to_port     = 443
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.elb.id}"]
+  }
 
-	egress {
-		from_port   = 0
-		to_port     = 65535
-		protocol    = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	tags {
-		Name = "app-george"
-	}
+  tags {
+    Name = "app-george"
+  }
 }
 
 # security group for the db
@@ -113,11 +171,11 @@ resource "aws_security_group" "db" {
   vpc_id      = "${aws_vpc.uat.id}"
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 27017
+    to_port     = 27017
     protocol    = "tcp"
-    # cidr_blocks = ["${aws_security_group.app.id}"]
     security_groups = ["${aws_security_group.app.id}"]
+    # cidr_block = "${aws_subnet.app.cidr_block}"
   }
 
   egress {
@@ -129,6 +187,39 @@ resource "aws_security_group" "db" {
 
   tags {
     Name = "db-george"
+  }
+}
+
+# security group for the elb
+resource "aws_security_group" "elb" {
+  name        = "elb-george"
+  description = "Security group for the elb"
+  vpc_id      = "${aws_vpc.uat.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # egress {
+  #   from_port   = 3000
+  #   to_port     = 3000
+  #   protocol    = "tcp"
+  #   # security_groups = ["${aws_security_group.app.id}"]
+  #   cidr_block = "${aws_subnet.app.cidr_block}"
+  # }
+
+  tags {
+    Name = "elb-george"
   }
 }
 
@@ -195,17 +286,28 @@ resource "aws_network_acl" "private" {
     rule_no    = 100
     action     = "allow"
     cidr_block = "0.0.0.0/0"
-    from_port  = 5432
-    to_port    = 5432
+    from_port  = 27017
+    to_port    = 27017
   }
+
+  ##########################################
+  egress {
+    protocol   = "tcp"
+    rule_no    = 50
+    action     = "allow"
+    cidr_block = "10.5.1.0/24"
+    from_port  = 0
+    to_port    = 65535
+  }
+  ##########################################
 
   egress {
     protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
     cidr_block = "0.0.0.0/0"
-    from_port  = 5432
-    to_port    = 5432
+    from_port  = 27017
+    to_port    = 27017
   }
 
   tags {
@@ -241,9 +343,20 @@ resource "aws_network_acl" "public" {
     rule_no    = 100
     action     = "allow"
     cidr_block = "10.5.1.0/24"
-    from_port  = 5432
-    to_port    = 5432
+    from_port  = 27017
+    to_port    = 27017
   }
+
+  ##########################################
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 50
+    action     = "allow"
+    cidr_block = "10.5.0.0/24"
+    from_port  = 0
+    to_port    = 65535
+  }
+  ##########################################
 
   egress {
     protocol   = "tcp"
@@ -254,34 +367,8 @@ resource "aws_network_acl" "public" {
     to_port    = 65535
   }
 
-  # egress {
-  #   protocol   = "tcp"
-  #   rule_no    = 300
-  #   action     = "allow"
-  #   cidr_block = "10.5.1.0/24"
-  #   from_port  = 5432
-  #   to_port    = 5432
-  # }
-
-  # egress {
-  #   protocol   = "tcp"
-  #   rule_no    = 200
-  #   action     = "allow"
-  #   cidr_block = "0.0.0.0/0"
-  #   from_port  = 443
-  #   to_port    = 443
-  # }
-
-  # egress {
-  #   protocol   = "tcp"
-  #   rule_no    = 100
-  #   action     = "allow"
-  #   cidr_block = "0.0.0.0/0"
-  #   from_port  = 80
-  #   to_port    = 80
-  # }
-
   tags {
     Name = "public-george"
   }
 }
+
